07c75c8010b07e990215a8116f5f84d7bd7920f8
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2006-10-23 01:22:00 +0000

    Infinite Impluse Response Filter$B$N%/%i%9$rDI2C!#(B
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@223 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ IIRFilter++.h
@@ -0,0 +1,431 @@
+/*
+ *  $Id: IIRFilter++.h,v 1.1 2006-10-23 01:22:00 ueshiba Exp $
+ */
+#ifndef __TUIIRFilterPP_h
+#define __TUIIRFilterPP_h
+
+#include "TU/Image++.h"
+
+namespace TU
+{
+/************************************************************************
+*  class IIRFilter							*
+************************************************************************/
+/*!
+  片側Infinite Inpulse Response Filterを表すクラス．
+*/
+template <u_int D> class IIRFilter
+{
+  public:
+    IIRFilter&	initialize(const float c[D+D])				;
+    template <class S> const IIRFilter&
+		forward(const Array<S>& in, Array<float>& out)	const	;
+    template <class S> const IIRFilter&
+		backward(const Array<S>& in, Array<float>& out)	const	;
+    void	limitsF(float& limit0F,
+			float& limit1F, float& limit2F)		const	;
+    void	limitsB(float& limit0B,
+			float& limit1B, float& limit2B)		const	;
+    
+  private:
+    float	_c[D+D];	// coefficients
+};
+
+/************************************************************************
+*  class BilateralIIRFilter						*
+************************************************************************/
+/*!
+  両側Infinite Inpulse Response Filterを表すクラス．
+*/
+template <u_int D> class BilateralIIRFilter
+{
+  public:
+  //! 微分の階数
+    enum Order
+    {
+	Zeroth,						//! 0階微分
+	First,						//! 1階微分
+	Second						//! 2階微分
+    };
+    
+    BilateralIIRFilter&	initialize(const float cF[D+D], const float cB[D+D]);
+    BilateralIIRFilter&	initialize(const float c[D+D], Order order)	;
+    template <class S>
+    BilateralIIRFilter&	convolve(const Array<S>& in)			;
+    u_int		dim()					const	;
+    float		operator [](int i)			const	;
+    void		limits(float& limit0,
+			       float& limit1,
+			       float& limit2)			const	;
+    
+  private:
+    IIRFilter<D>	_iirF;
+    Array<float>	_bufF;
+    IIRFilter<D>	_iirB;
+    Array<float>	_bufB;
+};
+
+//! フィルタのz変換係数をセットする
+/*!
+  \param cF	前進z変換係数. z変換は 
+		  Hf(w) =
+		      (cF[D-1] + cF[D-2]*w + cF[D-3]*w^2 + ... + cF[0]*w^(D-1))
+		    / (1 - cF[D+D-1]*w - cF[D+D-2]*w^2... - cF[D]*w^D)
+		となる. ただし w = z^(-1).
+  \param cB	後退z変換係数. z変換は
+		  Hb(z) = (cB[0]*z + cB[1]*z^2 + ... + cB[D-1]*z^D)
+		        / (1 - cB[D]*z - cB[D+1]*z^2... - cB[D+D-1]*z^D)
+		となる.
+*/
+template <u_int D> inline BilateralIIRFilter<D>&
+BilateralIIRFilter<D>::initialize(const float cF[D+D], const float cB[D+D])
+{
+    _iirF.initialize(cF);
+    _iirB.initialize(cB);
+#ifdef DEBUG
+    float	limit0, limit1, limit2;
+    limits(limit0, limit1, limit2);
+    std::cerr << "limit0 = " << limit0 << ", limit1 = " << limit1
+	      << ", limit2 = " << limit2 << std::endl;
+#endif
+    return *this;
+}
+
+//! フィルタによる畳み込みを行う. 出力は operator [](int) で取り出す
+/*!
+  \param in	入力データ列.
+  return	このフィルタ自身.
+*/
+template <u_int D> template <class S> inline BilateralIIRFilter<D>&
+BilateralIIRFilter<D>::convolve(const Array<S>& in)
+{
+    _iirF.forward(in, _bufF);
+    _iirB.backward(in, _bufB);
+
+    return *this;
+}
+
+//! 畳み込みの出力データ列の次元を返す
+/*!
+  \return	出力データ列の次元.
+*/
+template <u_int D> inline u_int
+BilateralIIRFilter<D>::dim() const
+{
+    return _bufF.dim();
+}
+
+//! 畳み込みの出力データの特定の要素を返す
+/*!
+  \param i	要素のindex.
+  \return	要素の値.
+*/
+template <u_int D> inline float
+BilateralIIRFilter<D>::operator [](int i) const
+{
+    return _bufF[i] + _bufB[i];
+}
+
+/************************************************************************
+*  class BilateralIIRFilter2						*
+************************************************************************/
+/*!
+  2次元両側Infinite Inpulse Response Filterを表すクラス．
+*/
+template <u_int D> class BilateralIIRFilter2
+{
+  public:
+    typedef typename BilateralIIRFilter<D>::Order	Order;
+    
+    BilateralIIRFilter2&
+		initialize(float cHF[D+D], float cHB[D+D],
+			   float cVF[D+D], float cVB[D+D])		;
+    BilateralIIRFilter2&
+		initialize(float cHF[D+D], Order orderH,
+			   float cVF[D+D], Order orderV)		;
+    template <class S, class T> BilateralIIRFilter2&
+		convolve(const Image<S>& in, Image<T>& out)		;
+    
+  private:
+    BilateralIIRFilter<D>	_iirH;
+    BilateralIIRFilter<D>	_iirV;
+    Array2<Array<float> >	_buf;
+};
+    
+//! フィルタのz変換係数をセットする
+/*!
+  \param cHF	横方向前進z変換係数.
+  \param cHB	横方向後退z変換係数.
+  \param cHV	縦方向前進z変換係数.
+  \param cHV	縦方向後退z変換係数.
+  \return	このフィルタ自身.
+*/
+template <u_int D> inline BilateralIIRFilter2<D>&
+BilateralIIRFilter2<D>::initialize(float cHF[D+D], float cHB[D+D],
+				   float cVF[D+D], float cVB[D+D])
+{
+    _iirH.initialize(cHF, cHB);
+    _iirV.initialize(cVF, cVB);
+
+    return *this;
+}
+
+//! フィルタのz変換係数をセットする
+/*!
+  \param cHF	横方向前進z変換係数.
+  \param orderH 横方向微分階数.
+  \param cHV	縦方向前進z変換係数.
+  \param orderV	縦方向微分階数.
+  \return	このフィルタ自身.
+*/
+template <u_int D> inline BilateralIIRFilter2<D>&
+BilateralIIRFilter2<D>::initialize(float cHF[D+D], Order orderH,
+				   float cVF[D+D], Order orderV)
+{
+    _iirH.initialize(cHF, orderH);
+    _iirV.initialize(cVF, orderV);
+
+    return *this;
+}
+
+/************************************************************************
+*  class DericheConvoler						*
+************************************************************************/
+/*!
+  Canny-Deriche核によるスムーシング，1次微分および2次微分を含む
+  画像畳み込みを行うクラス．
+*/
+class DericheConvolver : private BilateralIIRFilter2<2u>
+{
+  public:
+    using	BilateralIIRFilter2<2u>::Order;
+    
+    DericheConvolver(float alpha=1.0)		{initialize(alpha);}
+
+    DericheConvolver&	initialize(float alpha)				;
+    template <class S, class T>
+    DericheConvolver&	smooth(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    DericheConvolver&	gradH(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    DericheConvolver&	gradV(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    DericheConvolver&	laplacian(const Image<S>& in, Image<T>& out)	;
+
+  private:
+    float		_c0[4];	// forward coefficients for smoothing
+    float		_c1[4];	// forward coefficients for 1st derivatives
+    float		_c2[4];	// forward coefficients for 2nd derivatives
+    Image<float>	_tmp;	// buffer for storing intermediate values
+};
+
+//! Canny-Deriche核によるスムーシング
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このCanny-Deriche核自身.
+*/
+template <class S, class T> inline DericheConvolver&
+DericheConvolver::smooth(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<2u>::
+	initialize(_c0, BilateralIIRFilter<2u>::Zeroth,
+		   _c0, BilateralIIRFilter<2u>::Zeroth).convolve(in, out);
+
+    return *this;
+}
+
+//! Canny-Deriche核による横方向1次微分
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このCanny-Deriche核自身.
+*/
+template <class S, class T> inline DericheConvolver&
+DericheConvolver::gradH(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<2u>::
+	initialize(_c1, BilateralIIRFilter<2u>::First,
+		   _c0, BilateralIIRFilter<2u>::Zeroth).convolve(in, out);
+
+    return *this;
+}
+
+//! Canny-Deriche核による縦方向1次微分
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このCanny-Deriche核自身.
+*/
+template <class S, class T> inline DericheConvolver&
+DericheConvolver::gradV(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<2u>::
+	initialize(_c0, BilateralIIRFilter<2u>::Zeroth,
+		   _c1, BilateralIIRFilter<2u>::First).convolve(in, out);
+
+    return *this;
+}
+
+//! Canny-Deriche核によるラプラシアン
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このCanny-Deriche核自身.
+*/
+template <class S, class T> inline DericheConvolver&
+DericheConvolver::laplacian(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<2u>::
+	initialize(_c2, BilateralIIRFilter<2u>::Second,
+		   _c0, BilateralIIRFilter<2u>::Zeroth).convolve(in, _tmp).
+	initialize(_c0, BilateralIIRFilter<2u>::Zeroth,
+		   _c2, BilateralIIRFilter<2u>::Second).convolve(in, out);
+    out += _tmp;
+    
+    return *this;
+}
+
+/************************************************************************
+*  class GaussianConvoler						*
+************************************************************************/
+/*!
+  Gauss核によるスムーシング，1次微分(DOG)および2次微分(LOG)を含む
+  画像畳み込みを行うクラス．
+*/
+class GaussianConvolver : private BilateralIIRFilter2<4u>
+{
+  private:
+    struct Params
+    {
+	void		set(double aa, double bb, double tt, double aaa);
+	Params&		operator -=(const Vector<double>& p)		;
+    
+	double		a, b, theta, alpha;
+    };
+
+    class EvenConstraint
+    {
+      public:
+	typedef double		T;
+	typedef Array<Params>	AT;
+
+	EvenConstraint(T sigma) :_sigma(sigma)				{}
+	
+	Vector<T>	operator ()(const AT& params)		const	;
+	Matrix<T>	jacobian(const AT& params)		const	;
+
+      private:
+	T		_sigma;
+    };
+
+    class CostFunction
+    {
+      public:
+	typedef double		T;
+	typedef Array<Params>	AT;
+    
+	enum			{D = 2};
+
+	CostFunction(int ndivisions, T range)
+	    :_ndivisions(ndivisions), _range(range)			{}
+    
+	Vector<T>	operator ()(const AT& params)		const	;
+	Matrix<T>	jacobian(const AT& params)		const	;
+	void		update(AT& params, const Vector<T>& dp)	const	;
+
+      private:
+	const int	_ndivisions;
+	const T		_range;
+    };
+
+  public:
+    GaussianConvolver(float sigma=1.0)		{initialize(sigma);}
+
+    GaussianConvolver&	initialize(float sigma)				;
+    template <class S, class T>
+    GaussianConvolver&	smooth(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    GaussianConvolver&	gradH(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    GaussianConvolver&	gradV(const Image<S>& in, Image<T>& out)	;
+    template <class S, class T>
+    GaussianConvolver&	laplacian(const Image<S>& in, Image<T>& out)	;
+
+  private:
+    float		_c0[8];	// forward coefficients for smoothing
+    float		_c1[8];	// forward coefficients for 1st derivatives
+    float		_c2[8];	// forward coefficients for 2nd derivatives
+    Image<float>	_tmp;	// buffer for storing intermediate values
+};
+
+//! Gauss核によるスムーシング
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このGauss核自身.
+*/
+template <class S, class T> inline GaussianConvolver&
+GaussianConvolver::smooth(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<4u>::
+	initialize(_c0, BilateralIIRFilter<4u>::Zeroth,
+		   _c0, BilateralIIRFilter<4u>::Zeroth).convolve(in, out);
+
+    return *this;
+}
+
+//! Gauss核による横方向1次微分(DOG)
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このGauss核自身.
+*/
+template <class S, class T> inline GaussianConvolver&
+GaussianConvolver::gradH(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<4u>::
+	initialize(_c1, BilateralIIRFilter<4u>::First,
+		   _c0, BilateralIIRFilter<4u>::Zeroth).convolve(in, out);
+
+    return *this;
+}
+
+//! Gauss核による縦方向1次微分(DOG)
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このGauss核自身.
+*/
+template <class S, class T> inline GaussianConvolver&
+GaussianConvolver::gradV(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<4u>::
+	initialize(_c0, BilateralIIRFilter<4u>::Zeroth,
+		   _c1, BilateralIIRFilter<4u>::First).convolve(in, out);
+
+    return *this;
+}
+
+//! Gauss核によるラプラシアン(LOG)
+/*!
+  \param in	入力画像.
+  \param out	出力画像.
+  \return	このGauss核自身.
+*/
+template <class S, class T> inline GaussianConvolver&
+GaussianConvolver::laplacian(const Image<S>& in, Image<T>& out)
+{
+    BilateralIIRFilter2<4u>::
+	initialize(_c2, BilateralIIRFilter<4u>::Second,
+		   _c0, BilateralIIRFilter<4u>::Zeroth).convolve(in, _tmp).
+	initialize(_c0, BilateralIIRFilter<4u>::Zeroth,
+		   _c2, BilateralIIRFilter<4u>::Second).convolve(in, out);
+    out += _tmp;
+    
+    return *this;
+}
+
+}
+
+#endif	// !__TUIIRFilterPP_h
---
+++ IIRFilter.cc
@@ -0,0 +1,239 @@
+/*
+ *  $Id: IIRFilter.cc,v 1.1 2006-10-23 01:22:00 ueshiba Exp $
+ */
+#include <math.h>
+#include "TU/IIRFilter++.h"
+#include "TU/Minimize++.h"
+
+namespace TU
+{
+/************************************************************************
+*  static functions							*
+************************************************************************/
+static void
+coefficients4(float a0, float b0, float omega0, float alpha0,
+	      float a1, float b1, float omega1, float alpha1,
+	      float c[8])
+{
+    const float	c0 = cosf(omega0), s0 = sinf(omega0), e0 = expf(-alpha0),
+		c1 = cosf(omega1), s1 = sinf(omega1), e1 = expf(-alpha1);
+    c[0] =  e0*e1*(e1*(b0*s0 - a0*c0) + e0*(b1*s1 - a1*c1));	// i(n-3)
+    c[1] =  a0*e1*e1 + a1*e0*e0
+	 -  2.0*e0*e1*((b0*s0 - a0*c0)*c1 + (b1*s1 - a1*c1)*c0);// i(n-2)
+    c[2] =  e0*(b0*s0 - (a0 + 2.0*a1)*c0)
+	 +  e1*(b1*s1 - (a1 + 2.0*a0)*c1);			// i(n-1)
+    c[3] =  a0 + a1;						// i(n)
+    c[4] = -e0*e0*e1*e1;					// o(n-4)
+    c[5] =  2.0*e0*e1*(e1*c0 + e0*c1);				// o(n-3)
+    c[6] = -e0*e0 - e1*e1 - 4.0*e0*e1*c0*c1;			// o(n-2)
+    c[7] =  2.0*(e0*c0 + e1*c1);				// o(n-1)
+}
+
+/************************************************************************
+*  class DericheConvoler						*
+************************************************************************/
+//! このCanny-Deriche核の初期化を行う
+/*!
+  \param alpha	フィルタサイズを表す正数．小さいほど広がりが大きい．
+  \return	このCanny-Deriche核自身.
+*/
+DericheConvolver&
+DericheConvolver::initialize(float alpha)
+{
+    const float	e  = expf(-alpha), beta = sinhf(alpha);
+    _c0[0] =  (alpha - 1.0) * e;		// i(n-1)
+    _c0[1] =  1.0;				// i(n)
+    _c0[2] = -e * e;				// oF(n-2)
+    _c0[3] =  2.0 * e;				// oF(n-1)
+
+    _c1[0] = -1.0;				// i(n-1)
+    _c1[1] =  0.0;				// i(n)
+    _c1[2] = -e * e;				// oF(n-2)
+    _c1[3] =  2.0 * e;				// oF(n-1)
+
+    _c2[0] =  (1.0 + beta) * e;			// i(n-1)
+    _c2[1] = -1.0;				// i(n)
+    _c2[2] = -e * e;				// oF(n-2)
+    _c2[3] =  2.0 * e;				// oF(n-1)
+
+    return *this;
+}
+    
+/************************************************************************
+*  class GaussianConvoler::Params					*
+************************************************************************/
+void
+GaussianConvolver::Params::set(double aa, double bb, double tt, double aaa)
+{
+    a	  = aa;
+    b	  = bb;
+    theta = tt;
+    alpha = aaa;
+}
+
+GaussianConvolver::Params&
+GaussianConvolver::Params::operator -=(const Vector<double>& p)
+{
+    a	  -= p[0];
+    b	  -= p[1];
+    theta -= p[2];
+    alpha -= p[3];
+
+    return *this;
+}
+
+/************************************************************************
+*  class GaussianConvolver::EvenConstraint				*
+************************************************************************/
+Vector<double>
+GaussianConvolver::EvenConstraint::operator ()(const AT& params) const
+{
+    Vector<T>	val(1);
+    const T	as0 = params[0].alpha/_sigma, ts0 = params[0].theta/_sigma,
+		as1 = params[1].alpha/_sigma, ts1 = params[1].theta/_sigma;
+    val[0] = (params[0].a*sinh(as0) + params[0].b* sin(ts0)) *
+	     (cosh(as1) - cos(ts1))
+	   + (params[1].a*sinh(as1) + params[1].b* sin(ts1)) *
+	     (cosh(as0) - cos(ts0));
+
+    return val;
+}
+
+Matrix<double>
+GaussianConvolver::EvenConstraint::jacobian(const AT& params) const
+{
+    T	c0  = cos(params[0].theta/_sigma),  s0  = sin(params[0].theta/_sigma),
+	ch0 = cosh(params[0].alpha/_sigma), sh0 = sinh(params[0].alpha/_sigma),
+	c1  = cos(params[1].theta/_sigma),  s1  = sin(params[1].theta/_sigma),
+	ch1 = cosh(params[1].alpha/_sigma), sh1 = sinh(params[1].alpha/_sigma);
+	
+    Matrix<T>	val(1, 8);
+    val[0][0] = sh0*(ch1 - c1);
+    val[0][1] = s0 *(ch1 - c1);
+    val[0][2] = (params[0].b*c0 *(ch1 - c1) +
+		 s0 *(params[1].a*sh1 + params[1].b*s1)) / _sigma;
+    val[0][3] = (params[0].a*ch0*(ch1 - c1) +
+		 sh0*(params[1].a*sh1 + params[1].b*s1)) / _sigma;
+    val[0][4] = sh1*(ch0 - c0);
+    val[0][5] = s1 *(ch0 - c0);
+    val[0][6] = (params[1].b*c1 *(ch0 - c0) +
+		 s1 *(params[0].a*sh0 + params[0].b*s0)) / _sigma;
+    val[0][7] = (params[1].a*ch1*(ch0 - c0) +
+		 sh1*(params[0].a*sh0 + params[0].b*s0)) / _sigma;
+
+    return val;
+}
+
+/************************************************************************
+*  class GaussianConvolver::CostFunction				*
+************************************************************************/
+Vector<double>
+GaussianConvolver::CostFunction::operator ()(const AT& params) const
+{
+    Vector<T>	val(_ndivisions+1);
+    for (int k = 0; k < val.dim(); ++k)
+    {
+	T	f = 0.0, x = k*_range/_ndivisions;
+	for (int i = 0; i < params.dim(); ++i)
+	{
+	    const Params&	p = params[i];
+	    f += (p.a*cos(x*p.theta) + p.b*sin(x*p.theta))*exp(-x*p.alpha);
+	}
+	val[k] = f - (x*x - 1.0)*exp(-x*x/2.0);
+    }
+    
+    return val;
+}
+    
+Matrix<double>
+GaussianConvolver::CostFunction::jacobian(const AT& params) const
+{
+    Matrix<T>	val(_ndivisions+1, 4*params.dim());
+    for (int k = 0; k < val.nrow(); ++k)
+    {
+	Vector<T>&	row = val[k];
+	T		x = k*_range/_ndivisions;
+	
+	for (int i = 0; i < params.dim(); ++i)
+	{
+	    const Params&	p = params[i];
+	    const T		c = cos(x*p.theta), s = sin(x*p.theta),
+				e = exp(-x*p.alpha);
+	    row[4*i]   = c * e;
+	    row[4*i+1] = s * e;
+	    row[4*i+2] = (-p.a * s + p.b * c) * e;
+	    row[4*i+3] = -x * (p.a * c + p.b * s) * e;
+	}
+    }
+    
+    return val;
+}
+
+void
+GaussianConvolver::CostFunction::update(AT& params, const Vector<T>& dp) const
+{
+    for (int i = 0; i < params.dim(); ++i)
+	params[i] -= dp(4*i, 4);
+}
+
+/************************************************************************
+*  class GaussianConvolver						*
+************************************************************************/
+//! このGauss核の初期化を行う
+/*!
+  \param sigma	フィルタサイズを表す正数．大きいほど広がりが大きい．
+  \return	このGauss核自身.
+*/
+GaussianConvolver&
+GaussianConvolver::initialize(float sigma)
+{
+    coefficients4( 1.80579,   7.42555, 0.676413/sigma, 2.10032/sigma,
+		  -0.805838, -1.50785, 1.90174/sigma,  2.15811/sigma, _c0);
+    
+    coefficients4(-0.628422, -4.68837,  0.666686/sigma, 1.54201/sigma,
+		   0.628422,  0.980129, 2.08425/sigma,  1.52152/sigma, _c1);
+
+  /*    coefficients4(-1.27844,   3.24717, 0.752205/sigma, 1.18524/sigma,
+	0.278487, -1.54294, 2.21984/sigma,  1.27214/sigma, _c2);*/
+    CostFunction::AT	params(CostFunction::D);
+    params[0].set(-1.3, 3.6, 0.75, 1.2);
+    params[1].set(0.32, -1.7, 2.2, 1.3);
+    CostFunction	err(100, 5.0);
+    if (sigma < 0.55)
+	throw std::runtime_error("GaussianConvolver::initialize(): sigma must be greater than 0.55");
+    EvenConstraint	g(sigma);
+    minimizeSquare(err, g, params, 1000, 1.0e-6);
+    coefficients4(params[0].a, params[0].b,
+		  params[0].theta/sigma, params[0].alpha/sigma,
+		  params[1].a, params[1].b,
+		  params[1].theta/sigma, params[1].alpha/sigma, _c2);
+
+    return *this;
+}
+
+template class BilateralIIRFilter<2u>;
+
+template class BilateralIIRFilter2<2u>;
+template class BilateralIIRFilter2<2u>&
+BilateralIIRFilter2<2u>::convolve(const Image<u_char>& in, Image<u_char>& out);
+template BilateralIIRFilter2<2u>&
+BilateralIIRFilter2<2u>::convolve(const Image<u_char>& in, Image<float>& out);
+template BilateralIIRFilter2<2u>&
+BilateralIIRFilter2<2u>::convolve(const Image<float>& in, Image<float>& out);
+
+template class BilateralIIRFilter<4u>;
+
+template class BilateralIIRFilter2<4u>;
+template class BilateralIIRFilter2<4u>&
+BilateralIIRFilter2<4u>::convolve(const Image<u_char>& in, Image<u_char>& out);
+template BilateralIIRFilter2<4u>&
+BilateralIIRFilter2<4u>::convolve(const Image<u_char>& in, Image<float>& out);
+template BilateralIIRFilter2<4u>&
+BilateralIIRFilter2<4u>::convolve(const Image<float>& in, Image<float>& out);
+
+}
+
+#if defined(__GNUG__) || defined(__INTEL_COMPILER)
+#  include "TU/Array++.cc"
+#  include "TU/IIRFilter++.cc"
+#endif
