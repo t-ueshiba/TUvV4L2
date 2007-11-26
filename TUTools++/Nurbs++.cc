900690088b66efa7f7c48107a73cc8a1fd3fb392
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2007-11-26 07:55:48 +0000

    ����ｦ����
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@333 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUTools++/Nurbs++.cc
+++ TUTools++/Nurbs++.cc
@@ -1,30 +1,31 @@
 /*
- *  平成19年（独）産業技術総合研究所 著作権所有
+ *  平成9-19年（独）産業技術総合研究所 著作権所有
  *  
  *  創作者：植芝俊夫
  *
  *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
- *  同所が著作権を所有する秘密情報です．著作者による許可なしにこのプロ
- *  グラムを第三者へ開示，複製，改変，使用する等の著作権を侵害する行為
- *  を禁止します．
+ *  （独）産業技術総合研究所が著作権を所有する秘密情報です．創作者によ
+ *  る許可なしに本プログラムを使用，複製，改変，使用，第三者へ開示する
+ *  等の著作権を侵害する行為を禁止します．
  *  
- *  このプログラムによって生じるいかなる損害に対しても、著作者は責任
- *  を負いません。 
+ *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
+ *  よび創作者は責任を負いません。
  *
- *  Copyright 2007
+ *  Copyright 1997-2007.
  *  National Institute of Advanced Industrial Science and Technology (AIST)
  *
- *  Author: Toshio UESHIBA
+ *  Creator: Toshio UESHIBA
  *
- *  Confidentail and all rights reserved.
- *  This program is confidential. Any changing, copying or giving
- *  information about the source code of any part of this software
- *  and/or documents without permission by the authors are prohibited.
+ *  Confidential and all rights reserved.
+ *  This program is confidential. Any using, copying, changing, giving
+ *  information about the source program of any part of this software
+ *  to others without permission by the creators are prohibited.
  *
  *  No Warranty.
- *  Authors are not responsible for any damages in the use of this program.
+ *  Copyright holders or creators are not responsible for any damages
+ *  in the use of this program.
  *  
- *  $Id: Nurbs++.cc,v 1.9 2007-11-26 07:28:09 ueshiba Exp $
+ *  $Id: Nurbs++.cc,v 1.10 2007-11-26 07:55:48 ueshiba Exp $
  */
 #include "TU/utility.h"
 #include "TU/Nurbs++.h"
