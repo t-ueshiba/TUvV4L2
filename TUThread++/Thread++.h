513885ce17f4705ae3a1e2da9e7ee455d3889274
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2008-06-11 05:04:12 +0000

    Initial revision of libTUThread++.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@371 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ TUThread++/Thread++.h
@@ -0,0 +1,184 @@
+/*
+ *  平成14-19年（独）産業技術総合研究所 著作権所有
+ *  
+ *  創作者：植芝俊夫
+ *
+ *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
+ *  （独）産業技術総合研究所が著作権を所有する秘密情報です．創作者によ
+ *  る許可なしに本プログラムを使用，複製，改変，第三者へ開示する等の著
+ *  作権を侵害する行為を禁止します．
+ *  
+ *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
+ *  よび創作者は責任を負いません。
+ *
+ *  Copyright 2002-2007.
+ *  National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ *  Creator: Toshio UESHIBA
+ *
+ *  [AIST Confidential and all rights reserved.]
+ *  This program is confidential. Any using, copying, changing or
+ *  giving any information concerning with this program to others
+ *  without permission by the creator are strictly prohibited.
+ *
+ *  [No Warranty.]
+ *  The copyright holders or the creator are not responsible for any
+ *  damages in the use of this program.
+ *  
+ *  $Id: Thread++.h,v 1.1 2008-06-11 05:04:12 ueshiba Exp $
+ */
+#ifndef __TUThreadPP_h
+#define __TUThreadPP_h
+
+#include <pthread.h>
+#include "TU/Image++.h"
+
+namespace TU
+{
+/************************************************************************
+*  class Thread								*
+************************************************************************/
+class Thread
+{
+  public:
+    void		wait()					const	;
+    
+  protected:
+    Thread()								;
+    virtual ~Thread()							;
+
+    void		preRaise()				const	;
+    void		postRaise()				const	;
+
+  private:
+    virtual void	doJob()						= 0;
+    static void*	threadProc(void* thread)			;
+    
+    enum State		{Ready, Idle, Exit};
+
+    mutable pthread_mutex_t	_mutex;
+    mutable pthread_cond_t	_cond;
+    pthread_t			_thread;
+    mutable State		_state;
+};
+
+inline void
+Thread::preRaise() const
+{
+    pthread_mutex_lock(&_mutex);
+}
+
+inline void
+Thread::postRaise() const
+{
+    _state = Ready;
+    pthread_cond_signal(&_cond);	// Send Ready signal to the child.
+    pthread_mutex_unlock(&_mutex);
+}
+
+/************************************************************************
+*  class MultiThread<OP, IN, OUT>					*
+************************************************************************/
+template <class OP, class IN, class OUT>
+class MultiThread : public OP
+{
+  private:
+    class OperatorThread : public Thread
+    {
+      public:
+	OperatorThread()
+	    :Thread(), _op(0), _in(0), _out(0), _is(0), _ie(0)		{}
+
+	void		raise(const OP& op, const IN& in,
+			      OUT& out, int is, int ie)		const	;
+
+      private:
+	virtual void	doJob()						;
+
+	mutable const OP*	_op;
+	mutable const IN*	_in;
+	mutable OUT*		_out;
+	mutable int		_is, _ie;
+    };
+
+  public:
+    MultiThread(u_int nthreads=1)	:OP(), _threads(nthreads)	{}
+
+    void	createThreads(u_int nthreads)				;
+    void	operator ()(const IN& in, OUT& out)		const	;
+    
+  private:
+    void	raiseThreads(const IN& in, OUT& out)		const	;
+    
+    Array<OperatorThread>	_threads;
+};
+
+template <class OP, class IN, class OUT> inline void
+MultiThread<OP, IN, OUT>::createThreads(u_int nthreads)
+{
+    _threads.resize(nthreads);
+}
+
+template <class OP, class IN, class OUT> void
+MultiThread<OP, IN, OUT>::raiseThreads(const IN& in, OUT& out) const
+{
+    const int	d = out.dim() / _threads.dim();
+    for (int is = 0, n = 0; n < _threads.dim(); ++n)
+    {
+	const int	ie = (n < _threads.dim() - 1 ? is + d : out.dim());
+	_threads[n].raise(*this, in, out, is, ie);
+	is = ie;
+    }
+    for (int n = 0; n < _threads.dim(); ++n)
+	_threads[n].wait();
+}
+
+template <class OP, class IN, class OUT> inline void
+MultiThread<OP, IN, OUT>::OperatorThread::raise(const OP& op,
+						const IN& in, OUT& out,
+						int is, int ie) const
+{
+    preRaise();
+    _op  = &op;
+    _in  = &in;
+    _out = &out;
+    _is  = is;
+    _ie  = ie;
+    postRaise();
+}
+
+template <class OP, class IN, class OUT> inline void
+MultiThread<OP, IN, OUT>::OperatorThread::doJob()
+{
+    (*_op)(*_in, *_out, _is, _ie);
+}
+
+/************************************************************************
+*  class MultiThread<Warp, Image<T>, Image<T> >				*
+************************************************************************/
+template <> inline void
+MultiThread<Warp, Image<u_char>, Image<u_char> >::
+  operator ()(const Image<u_char>& in, Image<u_char>& out) const
+{
+    out.resize(height(), width());
+    raiseThreads(in, out);
+}
+    
+template <> inline void
+MultiThread<Warp, Image<RGBA>, Image<RGBA> >::
+  operator ()(const Image<RGBA>& in, Image<RGBA>& out) const
+{
+    out.resize(height(), width());
+    raiseThreads(in, out);
+}
+    
+template <> inline void
+MultiThread<Warp, Image<ABGR>, Image<ABGR> >::
+  operator ()(const Image<ABGR>& in, Image<ABGR>& out) const
+{
+    out.resize(height(), width());
+    raiseThreads(in, out);
+}
+    
+}
+#endif	// !__TUThreadPP_h
---
+++ TUThread++/Thread.cc
@@ -0,0 +1,96 @@
+/*
+ *  平成14-19年（独）産業技術総合研究所 著作権所有
+ *  
+ *  創作者：植芝俊夫
+ *
+ *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
+ *  （独）産業技術総合研究所が著作権を所有する秘密情報です．創作者によ
+ *  る許可なしに本プログラムを使用，複製，改変，第三者へ開示する等の著
+ *  作権を侵害する行為を禁止します．
+ *  
+ *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
+ *  よび創作者は責任を負いません。
+ *
+ *  Copyright 2002-2007.
+ *  National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ *  Creator: Toshio UESHIBA
+ *
+ *  [AIST Confidential and all rights reserved.]
+ *  This program is confidential. Any using, copying, changing or
+ *  giving any information concerning with this program to others
+ *  without permission by the creator are strictly prohibited.
+ *
+ *  [No Warranty.]
+ *  The copyright holders or the creator are not responsible for any
+ *  damages in the use of this program.
+ *  
+ *  $Id: Thread.cc,v 1.1 2008-06-11 05:04:12 ueshiba Exp $
+ */
+#include "TU/Thread++.h"
+#include <iostream>
+
+namespace TU
+{
+/************************************************************************
+*  class Thread								*
+************************************************************************/
+Thread::Thread()
+    :_mutex(), _cond(), _thread(), _state(Idle)
+{
+    pthread_mutex_init(&_mutex, NULL);
+    pthread_cond_init(&_cond, NULL);
+
+    pthread_create(&_thread, NULL, threadProc, this);
+}
+
+Thread::~Thread()
+{
+  // The Exit signal may be sent to the child thread before the child
+  // reach pthread_cond_wait() when ~Thread() is called immediately
+  // after Thread(). This results in the child failing to catch the
+  // signal and wait it forever! So, we need some delay here.
+    usleep(1000);
+    pthread_mutex_lock(&_mutex);
+    _state = Exit;
+    pthread_cond_signal(&_cond);	// Send Exit signal to the child.
+    pthread_mutex_unlock(&_mutex);
+
+    pthread_join(_thread, NULL);	// Wait the child terminating.
+    pthread_cond_destroy(&_cond);
+    pthread_mutex_destroy(&_mutex);
+}
+
+void
+Thread::wait() const
+{
+    pthread_mutex_lock(&_mutex);
+    while (_state != Idle)
+	pthread_cond_wait(&_cond, &_mutex);
+    pthread_mutex_unlock(&_mutex);
+}
+
+void*
+Thread::threadProc(void* t)
+{
+    Thread*	thread = (Thread*)t;
+
+    pthread_mutex_lock(&thread->_mutex);
+    for (;;)
+    {
+	pthread_cond_wait(&thread->_cond, &thread->_mutex);
+      	if (thread->_state == Ready)
+	{
+	    thread->doJob();				// Do main job.
+	    thread->_state = Idle;
+	    pthread_cond_signal(&thread->_cond);	// Send Idle signal
+	}						// to the parent.
+	else if (thread->_state == Exit)
+	    break;
+    }
+    pthread_mutex_unlock(&thread->_mutex);
+
+    return 0;
+}
+
+}
