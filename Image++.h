65375156c8cdd12acf1ebb2f102bc17f161851cd
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2008-06-11 05:02:43 +0000

    Image warping operator added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@369 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- Image++.h
+++ Image++.h
@@ -25,7 +25,7 @@
  *  The copyright holders or the creator are not responsible for any
  *  damages in the use of this program.
  *  
- *  $Id: Image++.h,v 1.33 2008-05-18 23:34:27 ueshiba Exp $
+ *  $Id: Image++.h,v 1.34 2008-06-11 05:02:42 ueshiba Exp $
  */
 #ifndef	__TUImagePP_h
 #define	__TUImagePP_h
@@ -1465,7 +1465,87 @@ DiagonalIntegralImage<T>::correct(int& u, int& v) const
 	u  = width() - 1;
     }
 }
+
+/************************************************************************
+*  class Warp								*
+************************************************************************/
+//! 画像を変形するためのクラス．
+class Warp
+{
+  private:
+    struct FracArray
+    {
+	FracArray(u_int d=0)
+	    :us(d), vs(d), du(d), dv(d), lmost(0)	{}
+
+	u_int		width()			const	{return us.dim();}
+	void		resize(u_int d)			;
+
+	Array<short>				us, vs;
+#ifdef __INTEL_COMPILER	
+	Array<u_char, AlignedBuf<u_char> >	du, dv;
+#else
+	Array<u_char>				du, dv;
+#endif
+	int					lmost;
+    };
     
+  public:
+    Warp()	:_fracs(), _width(0)			{}
+
+    u_int	width()				const	{return _width;}
+    u_int	height()			const	{return _fracs.dim();}
+    int		lmost(int v)			const	;
+    int		rmost(int v)			const	;
+
+    void	initialize(const Matrix<double>& Htinv,
+			   u_int inWidth,  u_int inHeight,
+			   u_int outWidth, u_int outHeight)		;
+    void	initialize(const Matrix<double>& Htinv,
+			   const CameraBase::Intrinsic& intrinsic,
+			   u_int inWidth,  u_int inHeight,
+			   u_int outWidth, u_int outHeight)		;
+    template <class T>
+    void	operator ()(const Image<T>& in, Image<T>& out,
+			    int vs=0, int ve=0)			const	;
+    Vector2f	operator ()(int u, int v)			const	;
+
+  private:
+    Array<FracArray>	_fracs;
+    u_int		_width;
+};
+
+inline void
+Warp::FracArray::resize(u_int d)
+{
+    us.resize(d);
+    vs.resize(d);
+    du.resize(d);
+    dv.resize(d);
+}
+
+inline int
+Warp::lmost(int v) const
+{
+    return _fracs[v].lmost;
+}
+
+inline int
+Warp::rmost(int v) const
+{
+    return _fracs[v].lmost + _fracs[v].width();
+}
+
+inline Vector2f
+Warp::operator ()(int u, int v) const
+{
+    Vector2f		val;
+    const FracArray&	fracs = _fracs[v];
+    val[0] = float(fracs.us[u]) + float(fracs.du[u]) / 128.0;
+    val[1] = float(fracs.vs[u]) + float(fracs.dv[u]) / 128.0;
+    return val;
+}
+
 }
 
 #endif	/* !__TUImagePP_h */
---
+++ Warp.cc
@@ -0,0 +1,374 @@
+/*
+ *  平成14-19年（独）産業技術総合研究所 著作権所有
+ *  
+ *  創作者：植芝俊夫
+ *
+ *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
+ *  （独）産業技術総合研究所が著作権を所有する秘密情報です．創作者によ
+ *  る許可なしに本プログラムを使用，複製，改変，第三者へ開示する等の著
+ *  作権を侵害する行為を禁止します．
+ *  
+ *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
+ *  よび創作者は責任を負いません。
+ *
+ *  Copyright 2002-2007.
+ *  National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ *  Creator: Toshio UESHIBA
+ *
+ *  [AIST Confidential and all rights reserved.]
+ *  This program is confidential. Any using, copying, changing or
+ *  giving any information concerning with this program to others
+ *  without permission by the creator are strictly prohibited.
+ *
+ *  [No Warranty.]
+ *  The copyright holders or the creator are not responsible for any
+ *  damages in the use of this program.
+ *  
+ *  $Id: Warp.cc,v 1.1 2008-06-11 05:02:43 ueshiba Exp $
+ */
+#include "TU/Image++.h"
+#if defined(__INTEL_COMPILER)
+#  undef SSE3
+#  undef SSE2
+#  undef SSE
+#  define MMX
+//#  define SSE
+//#  define SSE2
+//#  define SSE3
+#  include "TU/mmInstructions.h"
+#endif
+
+namespace TU
+{
+#if defined(MMX)
+  static inline mmInt	mmDup0(mmInt x)		{x = mmUnpackL8(x, x);
+						 return mmUnpackL(x, x);}
+  static inline mmInt	mmDup1(mmInt x)		{x = mmUnpackL8(x, x);
+						 return mmUnpackH(x, x);}
+  static inline mmInt	mmDup2(mmInt x)		{x = mmUnpackH8(x, x);
+						 return mmUnpackL(x, x);}
+  static inline mmInt	mmDup3(mmInt x)		{x = mmUnpackH8(x, x);
+						 return mmUnpackH(x, x);}
+#  if defined(SSE2)
+  static inline mmInt
+  mmLinearInterpolate(mmInt x, mmInt y, mmInt d)
+  {
+      return _mm_add_epi16(x, _mm_srai_epi16(
+			       _mm_mullo_epi16(d, _mm_sub_epi16(y, x)), 7));
+  }
+    
+  template <class T> static inline mmInt
+  mmBilinearInterpolate(const Image<T>& in, const short*& usp,
+			const short*& vsp, mmInt du, mmInt dv)
+  {
+      int	us0 = *usp++, us1 = *usp++, vs0 = *vsp++, vs1 = *vsp++;
+      mmInt	uc = _mm_set_epi32(*(int*)&in[vs1][us1+1],
+				   *(int*)&in[vs0][us0+1],
+				   *(int*)&in[vs1][us1],
+				   *(int*)&in[vs0][us0]);
+      mmInt	ss = mmLinearInterpolate(mmUnpackL8(uc), mmUnpackH8(uc), du);
+      ++vs0;
+      ++vs1;
+      uc = _mm_set_epi32(*(int*)&in[vs1][us1+1], *(int*)&in[vs0][us0+1],
+			 *(int*)&in[vs1][us1],   *(int*)&in[vs0][us0]);
+      return mmLinearInterpolate(ss, mmLinearInterpolate(mmUnpackL8(uc),
+							 mmUnpackH8(uc), du),
+				 dv);
+  }
+    
+  template <> inline mmInt
+  mmBilinearInterpolate(const Image<u_char>& in, const short*& usp,
+			const short*& vsp, mmInt du, mmInt dv)
+  {
+      int	us0 = *usp++, us1 = *usp++, us2 = *usp++, us3 = *usp++,
+		us4 = *usp++, us5 = *usp++, us6 = *usp++, us7 = *usp++,
+		vs0 = *vsp++, vs1 = *vsp++, vs2 = *vsp++, vs3 = *vsp++,
+		vs4 = *vsp++, vs5 = *vsp++, vs6 = *vsp++, vs7 = *vsp++;
+      mmInt	uc = _mm_set_epi8(in[vs7][us7+1], in[vs6][us6+1],
+				  in[vs5][us5+1], in[vs4][us4+1],
+				  in[vs3][us3+1], in[vs2][us2+1],
+				  in[vs1][us1+1], in[vs0][us0+1],
+				  in[vs7][us7],   in[vs6][us6],
+				  in[vs5][us5],   in[vs4][us4],
+				  in[vs3][us3],   in[vs2][us2],
+				  in[vs1][us1],   in[vs0][us0]);
+      mmInt	ss = mmLinearInterpolate(mmUnpackL8(uc),
+					 mmUnpackH8(uc), du);
+      ++vs0;
+      ++vs1;
+      ++vs2;
+      ++vs3;
+      ++vs4;
+      ++vs5;
+      ++vs6;
+      ++vs7;
+      uc = _mm_set_epi8(in[vs7][us7+1], in[vs6][us6+1],
+			in[vs5][us5+1], in[vs4][us4+1],
+			in[vs3][us3+1], in[vs2][us2+1],
+			in[vs1][us1+1], in[vs0][us0+1],
+			in[vs7][us7],   in[vs6][us6],
+			in[vs5][us5],   in[vs4][us4],
+			in[vs3][us3],   in[vs2][us2],
+			in[vs1][us1],   in[vs0][us0]);
+      return mmLinearInterpolate(ss, mmLinearInterpolate(mmUnpackL8(uc),
+							 mmUnpackH8(uc), du),
+				 dv);
+  }
+#  else
+  static inline mmInt
+  mmLinearInterpolate(mmInt x, mmInt y, mmInt d)
+  {
+      return _mm_add_pi16(x, _mm_srai_pi16(
+			      _mm_mullo_pi16(d, _mm_sub_pi16(y, x)), 7));
+  }
+
+  template <class T> static inline mmInt
+  mmBilinearInterpolate(const Image<T>& in, const short*& usp,
+			const short*& vsp, mmInt du, mmInt dv)
+  {
+      int	us0 = *usp++, vs0 = *vsp++;
+      mmInt	uc = _mm_set_pi32(*(int*)&in[vs0][us0+1],
+				  *(int*)&in[vs0][us0]);
+      mmInt	ss = mmLinearInterpolate(mmUnpackL8(uc), mmUnpackH8(uc), du);
+      ++vs0;
+      uc = _mm_set_pi32(*(int*)&in[vs0][us0+1], *(int*)&in[vs0][us0]);
+      return mmLinearInterpolate(ss, mmLinearInterpolate(mmUnpackL8(uc),
+							 mmUnpackH8(uc), du),
+				 dv);
+  }
+
+  template <> inline mmInt
+  mmBilinearInterpolate(const Image<u_char>& in, const short*& usp,
+			 const short*& vsp, mmInt du, mmInt dv)
+  {
+      int	us0 = *usp++, us1 = *usp++, us2 = *usp++, us3 = *usp++,
+		vs0 = *vsp++, vs1 = *vsp++, vs2 = *vsp++, vs3 = *vsp++;
+      mmInt	uc = _mm_set_pi8(in[vs3][us3+1], in[vs2][us2+1],
+				 in[vs1][us1+1], in[vs0][us0+1],
+				 in[vs3][us3],   in[vs2][us2],
+				 in[vs1][us1],   in[vs0][us0]);
+      mmInt	ss = mmLinearInterpolate(mmUnpackL8(uc), mmUnpackH8(uc), du);
+      ++vs0;
+      ++vs1;
+      ++vs2;
+      ++vs3;
+      uc = _mm_set_pi8(in[vs3][us3+1], in[vs2][us2+1],
+		       in[vs1][us1+1], in[vs0][us0+1],
+		       in[vs3][us3],   in[vs2][us2],
+		       in[vs1][us1],   in[vs0][us0]);
+      return mmLinearInterpolate(ss, mmLinearInterpolate(mmUnpackL8(uc),
+							 mmUnpackH8(uc), du),
+				 dv);
+  }
+#  endif
+#endif
+    
+/************************************************************************
+*  static functions							*
+************************************************************************/
+template <class T> static inline T
+bilinearInterpolate(const Image<T>& in, int us, int vs, int du, int dv)
+{
+    T		in00 = in[vs][us],   in01 = in[vs][us+1],
+		in10 = in[vs+1][us], in11 = in[vs+1][us+1];
+    int		tmp0, tmp1;
+    T		out;
+    tmp0 = int(in00.r) + ((du * (int(in01.r) - int(in00.r))) >> 7);
+    tmp1 = int(in10.r) + ((du * (int(in11.r) - int(in10.r))) >> 7);
+    out.r = tmp0 + ((dv * (tmp1 - tmp0)) >> 7);
+    tmp0 = int(in00.g) + ((du * (int(in01.g) - int(in00.g))) >> 7);
+    tmp1 = int(in10.g) + ((du * (int(in11.g) - int(in10.g))) >> 7);
+    out.g = tmp0 + ((dv * (tmp1 - tmp0)) >> 7);
+    tmp0 = int(in00.b) + ((du * (int(in01.b) - int(in00.b))) >> 7);
+    tmp1 = int(in10.b) + ((du * (int(in11.b) - int(in10.b))) >> 7);
+    out.b = tmp0 + ((dv * (tmp1 - tmp0)) >> 7);
+    tmp0 = int(in00.a) + ((du * (int(in01.a) - int(in00.a))) >> 7);
+    tmp1 = int(in10.a) + ((du * (int(in11.a) - int(in10.a))) >> 7);
+    out.a = tmp0 + ((dv * (tmp1 - tmp0)) >> 7);
+
+    return out;
+}
+
+template <> inline u_char
+bilinearInterpolate(const Image<u_char>& in, int us, int vs, int du, int dv)
+{
+    int		in00 = in[vs][us],   in01 = in[vs][us+1],
+		in10 = in[vs+1][us], in11 = in[vs+1][us+1];
+    int		out0 = in00 + ((du * (in01 - in00)) >> 7),
+		out1 = in10 + ((du * (in11 - in10)) >> 7);
+    
+    return out0 + ((dv * (out1 - out0)) >> 7);
+}
+
+/************************************************************************
+*  class Warp								*
+************************************************************************/
+void
+Warp::initialize(const Matrix<double>& Htinv,
+		 u_int inWidth,  u_int inHeight,
+		 u_int outWidth, u_int outHeight)
+{
+    initialize(Htinv, CameraWithDistortion::Intrinsic(),
+	       inWidth, inHeight, outWidth, outHeight);
+}
+
+void
+Warp::initialize(const Matrix<double>& Htinv,
+		 const CameraBase::Intrinsic& intrinsic,
+		 u_int inWidth,  u_int inHeight,
+		 u_int outWidth, u_int outHeight)
+{
+    _fracs.resize(outHeight);
+    _width = outWidth;
+    
+  /* Compute frac for each pixel. */
+    const Matrix<double>&	HKtinv = Htinv * intrinsic.Ktinv();
+    Vector<double>		leftmost = HKtinv[2];
+    for (int v = 0; v < height(); ++v)
+    {
+	Vector<double>	xc = leftmost;
+	FracArray	frac(width());
+	int		n = 0;
+	for (int u = 0; u < width(); ++u)
+	{
+	    const Point2<double>&
+		m = intrinsic(Point2<double>(xc[0]/xc[2], xc[1]/xc[2]));
+	    if (0.0 <= m[0] && m[0] <= inWidth - 2 &&
+		0.0 <= m[1] && m[1] <= inHeight - 2)
+	    {
+		if (n == 0)
+		    frac.lmost = u;
+		frac.us[n] = (short)floor(m[0]);
+		frac.vs[n] = (short)floor(m[1]);
+		frac.du[n] = (u_char)floor((m[0] - floor(m[0])) * 128.0);
+		frac.dv[n] = (u_char)floor((m[1] - floor(m[1])) * 128.0);
+		++n;
+	    }
+	    xc += HKtinv[0];
+	}
+
+	_fracs[v].resize(n);
+	_fracs[v].lmost = frac.lmost;
+	for (int u = 0; u < n; ++u)
+	{
+	    _fracs[v].us[u] = frac.us[u];
+	    _fracs[v].vs[u] = frac.vs[u];
+	    _fracs[v].du[u] = frac.du[u];
+	    _fracs[v].dv[u] = frac.dv[u];
+	}
+
+	leftmost += HKtinv[1];
+    }
+}
+
+template <class T> void
+Warp::operator ()(const Image<T>& in, Image<T>& out, int vs, int ve) const
+{
+    out.resize(height(), width());
+    if (ve == 0)
+	ve = out.height();
+    
+    for (int v = vs; v < ve; ++v)
+    {
+	const short	*usp  = _fracs[v].us, *vsp  = _fracs[v].vs;
+	const u_char	*dup  = _fracs[v].du, *dvp  = _fracs[v].dv;
+	T		*outp = out[v] + _fracs[v].lmost;
+	T* const	outq  = outp + _fracs[v].width();
+#if defined(MMX)
+	for (T* const outr = outq - mmNBytes; outp <= outr; )
+	{
+	    mmInt	du = mmLoad((mmInt*)dup), dv = mmLoad((mmInt*)dvp);
+	    mmInt	du4 = mmDup0(du), dv4 = mmDup0(dv);
+	    mmInt	out0 = mmBilinearInterpolate(in, usp, vsp,
+						     mmUnpackL8(du4),
+						     mmUnpackL8(dv4)),
+			out1 = mmBilinearInterpolate(in, usp, vsp,
+						     mmUnpackH8(du4),
+						     mmUnpackH8(dv4));
+	    mmStoreU((mmInt*)outp, mmPackUS(out0, out1));
+	    outp += mmNDWords;
+	    du4 = mmDup1(du), dv4 = mmDup1(dv);
+	    out0 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackL8(du4), mmUnpackL8(dv4));
+	    out1 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackH8(du4), mmUnpackH8(dv4));
+	    mmStoreU((mmInt*)outp, mmPackUS(out0, out1));
+	    outp += mmNDWords;
+	    du4 = mmDup2(du), dv4 = mmDup2(dv);
+	    out0 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackL8(du4), mmUnpackL8(dv4));
+	    out1 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackH8(du4), mmUnpackH8(dv4));
+	    mmStoreU((mmInt*)outp, mmPackUS(out0, out1));
+	    outp += mmNDWords;
+	    du4 = mmDup3(du), dv4 = mmDup3(dv);
+	    out0 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackL8(du4), mmUnpackL8(dv4));
+	    out1 = mmBilinearInterpolate(in, usp, vsp,
+					 mmUnpackH8(du4), mmUnpackH8(dv4));
+	    mmStoreU((mmInt*)outp, mmPackUS(out0, out1));
+	    outp += mmNDWords;
+
+	    dup += mmNBytes;
+	    dvp += mmNBytes;
+	}
+#endif
+      	while (outp < outq)
+	    *outp++ = bilinearInterpolate(in, *usp++, *vsp++, *dup++, *dvp++);
+	out[v].setLimits(_fracs[v].lmost, _fracs[v].lmost + _fracs[v].width());
+    }
+#if defined(MMX) && !defined(SSE2)
+    _mm_empty();
+#endif	
+}
+
+template <> void
+Warp::operator ()(const Image<u_char>& in, Image<u_char>& out,
+		  int vs, int ve) const
+{
+    out.resize(height(), width());
+    if (ve == 0)
+	ve = out.height();
+    
+    for (int v = vs; v < ve; ++v)
+    {
+	const short	*usp  = _fracs[v].us, *vsp  = _fracs[v].vs;
+	const u_char	*dup  = _fracs[v].du, *dvp  = _fracs[v].dv;
+	u_char		*outp = out[v] + _fracs[v].lmost;
+	u_char* const	outq  = outp + _fracs[v].width();
+#if defined(MMX)
+	for (u_char* const outr = outq - mmNBytes; outp <= outr; )
+	{
+	    mmInt	du = mmLoad((mmInt*)dup), dv = mmLoad((mmInt*)dvp);
+	    mmInt	out0 = mmBilinearInterpolate(in, usp, vsp,
+						     mmUnpackL8(du),
+						     mmUnpackL8(dv)),
+			out1 = mmBilinearInterpolate(in, usp, vsp,
+						     mmUnpackH8(du),
+						     mmUnpackH8(dv));
+	    mmStoreU((mmInt*)outp, mmPackUS(out0, out1));
+
+	    outp += mmNBytes;
+	    dup  += mmNBytes;
+	    dvp  += mmNBytes;
+	}
+#endif
+      	while (outp < outq)
+	    *outp++ = bilinearInterpolate(in, *usp++, *vsp++, *dup++, *dvp++);
+	out[v].setLimits(_fracs[v].lmost, _fracs[v].lmost + _fracs[v].width());
+    }
+#if defined(MMX) && !defined(SSE2)
+    _mm_empty();
+#endif	
+}
+
+template void	Warp::operator ()(const Image<RGBA>& in,
+				  Image<RGBA>& out, int vs, int ve) const;
+template void	Warp::operator ()(const Image<ABGR>& in,
+				  Image<ABGR>& out, int vs, int ve) const;
+}
+
+#if defined(__GNUG__) || defined(__INTEL_COMPILER)
+#  include "TU/Image++.cc"
+#endif
