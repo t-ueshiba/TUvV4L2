32f9fc3d07f43bd863ba7473cff2f66b0c910d8a
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2011-01-05 02:04:50 +0000

    TriggerGenerator�������������＜������О����眼�
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@899 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUIeee1394++/testcam2/MyCmdWindow.h
+++ TUIeee1394++/testcam2/MyCmdWindow.h
@@ -1,5 +1,5 @@
 /*
- *  $Id: MyCmdWindow.h,v 1.2 2010-12-28 11:48:41 ueshiba Exp $
+ *  $Id: MyCmdWindow.h,v 1.3 2011-01-05 02:04:50 ueshiba Exp $
  */
 #include "TU/v/App.h"
 #include "TU/v/CmdWindow.h"
@@ -8,7 +8,7 @@
 #include "TU/v/Timer.h"
 #include "MyCanvasPane.h"
 #include "TU/Ieee1394++.h"
-#include "TU/Serial.h"
+#include "TU/TriggerGenerator.h"
 #include "TU/Movie.h"
 
 namespace TU
