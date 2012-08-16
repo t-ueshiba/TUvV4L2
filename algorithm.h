7c9ae9cb5e399a2385554238da4d11d29851a7ff
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2012-08-16 04:00:42 +0000

    Document added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1153 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- algorithm.h
+++ algorithm.h
@@ -1,5 +1,31 @@
 /*
- *  $Id: algorithm.h,v 1.2 2012-08-16 02:20:20 ueshiba Exp $
+ *  平成14-24年（独）産業技術総合研究所 著作権所有
+ *  
+ *  創作者：植芝俊夫
+ *
+ *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
+ *  （独）産業技術総合研究所が著作権を所有する秘密情報です．著作権所有
+ *  者による許可なしに本プログラムを使用，複製，改変，第三者へ開示する
+ *  等の行為を禁止します．
+ *  
+ *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
+ *  よび創作者は責任を負いません。
+ *
+ *  Copyright 2002-2012.
+ *  National Institute of Advanced Industrial Science and Technology (AIST)
+ *
+ *  Creator: Toshio UESHIBA
+ *
+ *  [AIST Confidential and all rights reserved.]
+ *  This program is confidential. Any using, copying, changing or
+ *  giving any information concerning with this program to others
+ *  without permission by the copyright holder are strictly prohibited.
+ *
+ *  [No Warranty.]
+ *  The copyright holder or the creator are not responsible for any
+ *  damages caused by using this program.
+ *  
+ *  $Id: algorithm.h,v 1.3 2012-08-16 04:00:42 ueshiba Exp $
  */
 /*!
   \file		algorithm.h
