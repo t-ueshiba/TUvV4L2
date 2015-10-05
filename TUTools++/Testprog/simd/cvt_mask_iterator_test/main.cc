0fd090209d56708dd775ec6a4bdc7c2d30c5e3e6
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2015-10-05 19:46:49 +0000

    Test programs for simd added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1864 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ TUTools++/Testprog/simd/cvt_mask_iterator_test/main.cc
@@ -0,0 +1,173 @@
+/*
+ *  $Id$
+ */
+#include <iomanip>
+#include "TU/simd/simd.h"
+
+namespace TU
+{
+namespace simd
+{
+template <class SRC, class DST> void
+doJob()
+{
+    using namespace	std;
+    using namespace	simd;
+    
+    typedef SRC						src_type;
+    typedef DST						dst_type;
+    typedef load_iterator<const src_type*>		siterator;
+    typedef store_iterator<dst_type*>			diterator;
+    typedef typename boost::mpl::if_c<
+	vec<src_type>::size <= vec<dst_type>::size,
+	cvtdown_mask_iterator<dst_type, siterator>,
+	siterator>::type				src_iterator;
+    typedef typename boost::mpl::if_c<
+	vec<src_type>::size <= vec<dst_type>::size,
+	diterator,
+	cvtup_mask_iterator<diterator> >::type		dst_iterator;
+
+    u_char	p[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+		       0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+		       0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+		       0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f};
+    dst_type	dst[32];
+    size_t	n = sizeof(p)/sizeof(src_type);
+    
+    copy(src_iterator((const src_type*)p),
+	 src_iterator((const src_type*)p + n), dst_iterator(dst));
+
+    empty();
+    
+    for (const dst_type* q = dst; q != dst + n; ++q)
+	cout << ' ' << setfill('0') << setw(2*sizeof(dst_type)) << hex
+	     << (u_int64_t(*q) & (u_int64_t(~0) >> (64 - 8*sizeof(dst_type))));
+    cout << endl;
+}
+    
+}
+}
+
+int
+main()
+{
+    using namespace	std;
+    using namespace	TU;
+
+    cerr << "--- src: int8_t, dst: singed ---" << endl;
+  //simd::doJob<int8_t,	 int8_t >();
+    simd::doJob<int8_t,	 int16_t>();
+    simd::doJob<int8_t,	 int32_t>();
+    simd::doJob<int8_t,	 int64_t>();
+
+    cerr << "--- src: int8_t, dst: unsinged ---" << endl;
+  //simd::doJob<int8_t,	 u_int8_t >();
+    simd::doJob<int8_t,	 u_int16_t>();
+    simd::doJob<int8_t,	 u_int32_t>();
+    simd::doJob<int8_t,	 u_int64_t>();
+
+    cerr << "--- src: u_int8_t, dst: singed ---" << endl;
+  //simd::doJob<u_int8_t,  int8_t >();
+    simd::doJob<u_int8_t,  int16_t>();
+    simd::doJob<u_int8_t,  int32_t>();
+    simd::doJob<u_int8_t,  int64_t>();
+
+    cerr << "--- src: u_int8_t, dst: unsinged ---" << endl;
+  //simd::doJob<u_int8_t,  u_int8_t >();
+    simd::doJob<u_int8_t,  u_int16_t>();
+    simd::doJob<u_int8_t,  u_int32_t>();
+    simd::doJob<u_int8_t,  u_int64_t>();
+
+    cerr << "--- src: int16_t, dst: singed ---" << endl;
+    simd::doJob<int16_t,   int8_t >();
+    simd::doJob<int16_t,   int16_t>();
+    simd::doJob<int16_t,   int32_t>();
+    simd::doJob<int16_t,   int64_t>();
+
+    cerr << "--- src: int16_t, dst: unsinged ---" << endl;
+    simd::doJob<int16_t,   u_int8_t >();
+    simd::doJob<int16_t,   u_int16_t>();
+    simd::doJob<int16_t,   u_int32_t>();
+    simd::doJob<int16_t,   u_int64_t>();
+
+    cerr << "--- src: u_int16_t, dst: singed ---" << endl;
+    simd::doJob<u_int16_t, int8_t >();
+    simd::doJob<u_int16_t, int16_t>();
+    simd::doJob<u_int16_t, int32_t>();
+    simd::doJob<u_int16_t, int64_t>();
+
+    cerr << "--- src: u_int16_t, dst: unsinged ---" << endl;
+    simd::doJob<u_int16_t, u_int8_t >();
+    simd::doJob<u_int16_t, u_int16_t>();
+    simd::doJob<u_int16_t, u_int32_t>();
+    simd::doJob<u_int16_t, u_int64_t>();
+
+    cerr << "--- src: int32_t, dst: singed ---" << endl;
+    simd::doJob<int32_t,   int8_t >();
+    simd::doJob<int32_t,   int16_t>();
+    simd::doJob<int32_t,   int32_t>();
+    simd::doJob<int32_t,   int64_t>();
+
+    cerr << "--- src: int32_t, dst: unsinged ---" << endl;
+    simd::doJob<int32_t,   u_int8_t >();
+    simd::doJob<int32_t,   u_int16_t>();
+    simd::doJob<int32_t,   u_int32_t>();
+    simd::doJob<int32_t,   u_int64_t>();
+
+    cerr << "--- src: u_int32_t, dst: singed ---" << endl;
+    simd::doJob<u_int32_t, int8_t >();
+    simd::doJob<u_int32_t, int16_t>();
+    simd::doJob<u_int32_t, int32_t>();
+    simd::doJob<u_int32_t, int64_t>();
+
+    cerr << "--- src: u_int32_t, dst: unsinged ---" << endl;
+    simd::doJob<u_int32_t, u_int8_t >();
+    simd::doJob<u_int32_t, u_int16_t>();
+  //simd::doJob<u_int32_t, u_int32_t>();
+    simd::doJob<u_int32_t, u_int64_t>();
+
+    cerr << "--- src: int64_t ---" << endl;
+  //simd::doJob<int64_t,   int64_t>();
+    simd::doJob<int64_t,   u_int64_t>();
+    cerr << "--- src: u_int64_t ---" << endl;
+    simd::doJob<u_int64_t, int64_t>();
+  //simd::doJob<u_int64_t, u_int64_t>();
+
+#if defined(SSE2)
+    simd::doJob<int8_t,    float    >();
+    simd::doJob<int16_t,   float    >();
+
+    simd::doJob<u_int8_t,  float    >();
+    simd::doJob<u_int16_t, float    >();
+
+    simd::doJob<float,     int16_t  >();
+    simd::doJob<float,     int8_t   >();
+
+    simd::doJob<float,     u_int16_t>();
+    simd::doJob<float,     u_int8_t >();
+
+    simd::doJob<int8_t,    double   >();
+    simd::doJob<int16_t,   double   >();
+    simd::doJob<int32_t,   double   >();
+
+    simd::doJob<u_int8_t,  double   >();
+    simd::doJob<u_int16_t, double   >();
+    simd::doJob<u_int32_t, double   >();
+
+#  if defined(AVX2) || !defined(AVX)
+    simd::doJob<int32_t,   float    >();
+    simd::doJob<u_int32_t, float    >();
+
+    simd::doJob<float,     int32_t  >();
+    simd::doJob<float,     u_int32_t>();
+
+    simd::doJob<int64_t,   double   >();
+    simd::doJob<u_int64_t, double   >();
+
+    simd::doJob<double,    int64_t  >();
+    simd::doJob<double,    u_int64_t>();
+#  endif
+#endif
+
+    return 0;
+}
