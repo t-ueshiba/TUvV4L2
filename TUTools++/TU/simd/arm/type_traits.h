34a645f126ca7167a4b3082a7972c87c25bb52a1
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2015-10-07 08:24:55 +0000

    Cleaning up.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1868 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUTools++/TU/simd/arm/type_traits.h
+++ TUTools++/TU/simd/arm/type_traits.h
@@ -18,8 +18,6 @@ template <class T>	struct type_traits;
 template <>
 struct type_traits<int8_t> : type_traits_base<int8_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef int8x8_t		half_type;
     typedef int8x16_t		base_type;
 };
@@ -27,8 +25,6 @@ struct type_traits<int8_t> : type_traits_base<int8_t>
 template <>
 struct type_traits<int16_t> : type_traits_base<int16_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef int16x4_t		half_type;
     typedef int16x8_t		base_type;
 };
@@ -36,8 +32,6 @@ struct type_traits<int16_t> : type_traits_base<int16_t>
 template <>
 struct type_traits<int32_t> : type_traits_base<int32_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef int32x2_t		half_type;
     typedef int32x4_t		base_type;
 };
@@ -54,8 +48,6 @@ struct type_traits<int64_t> : type_traits_base<int64_t>
 template <>
 struct type_traits<u_int8_t> : type_traits_base<u_int8_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef uint8x8_t		half_type;
     typedef uint8x16_t		base_type;
 };
@@ -63,8 +55,6 @@ struct type_traits<u_int8_t> : type_traits_base<u_int8_t>
 template <>
 struct type_traits<u_int16_t> : type_traits_base<u_int16_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef uint16x4_t		half_type;
     typedef uint16x8_t		base_type;
 };
@@ -72,8 +62,6 @@ struct type_traits<u_int16_t> : type_traits_base<u_int16_t>
 template <>
 struct type_traits<u_int32_t> : type_traits_base<u_int32_t>
 {
-    typedef float		complementary_type;
-    typedef complementary_type	complementary_mask_type;
     typedef uint32x2_t		half_type;
     typedef uint32x4_t		base_type;
 };
