b2662fd324af40524c3d6e586db811e602724f1e
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2016-01-26 07:45:29 +0000

    dup.h added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1920 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ TU/simd/arm/dup.h
@@ -0,0 +1,17 @@
+/*
+ *  $Id$
+ */
+#if !defined(__TU_SIMD_ARM_DUP_H)
+#define __TU_SIMD_ARM_DUP_H
+
+namespace TU
+{
+namespace simd
+{
+/************************************************************************
+*  Duplication operators						*
+************************************************************************/
+    
+}	// namespace simd
+}	// namespace TU
+#endif	// !__TU_SIMD_ARM_DUP_H
