376a61854d8f30205115978954c517c6d04810fd
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2016-01-29 21:33:48 +0000

    New template parameter ASSIGN has been added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1928 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUTools++/TU/simd/transform.h
+++ TUTools++/TU/simd/transform.h
@@ -18,12 +18,14 @@ namespace detail
     入力反復子毎に異なるSIMDベクトルの型をvec<T>型に統一して変換関数に入力し，
     その結果を再び変換して出力反復子に書き出す．
     \param T		関数に入力するSIMDベクトルの要素型
+    \param ASSIGN	変換結果を出力反復子に書き出す際に用いる代入演算子
     \param MASK		型変換をマスクベクトルとして行うならtrue, そうでなければfalse
     \param ITER_TUPLE	複数の入力反復子をまとめたタプル
     \param OUT		関数が出力するSIMDベクトルの変換結果を書き出す出力反復子
     \param FUNC		変換関数
   */
-  template <class T, bool MASK, class ITER_TUPLE, class OUT, class FUNC>
+  template <class T, class ASSIGN, bool MASK,
+	    class ITER_TUPLE, class OUT, class FUNC>
   class transformer
   {
     public:
@@ -46,20 +48,14 @@ namespace detail
 			typename std::iterator_traits<OUT>::value_type> >::type;
       
     //! 出力反復子に書き出すSIMDベクトルの要素型
-      using R = typename std::conditional<std::is_void<E>::value, T, E>::type;
+      using O = typename std::conditional<std::is_void<E>::value, T, E>::type;
     //! 変換関数に入力するSIMDベクトルの要素型
-      using U = typename std::conditional<std::is_void<T>::value, R, T>::type;
-    //! vec<S> を vec<R> に変換する過程において vec<S> の直後の変換先の要素型
-      template <class S>
-      using below_type =
-	  typename std::conditional<
-		       (vec<cvt_lower_type<R, S, MASK> >::size > vec<S>::size),
-		       S, cvt_lower_type<R, S, MASK> >::type;
+      using I = typename std::conditional<std::is_void<T>::value, O, T>::type;
 
-    //! vec<U> よりも要素数が少ない入力SIMDベクトルを vec<U> に変換
-      struct generic_cvtdown
+    //! vec<I> よりも要素数が少ない入力SIMDベクトルを vec<I> に変換
+      struct generic_downArg
       {
-	  template <class T_=U, class ITER_>
+	  template <class T_=I, class ITER_>
 	  typename std::enable_if<
 	      (vec<T_>::size == std::iterator_traits<ITER_>::value_type::size),
 	      vec<T_> >::type
@@ -69,7 +65,7 @@ namespace detail
 			    ++iter;
 			    return cvt<T_, false, MASK>(x);
 			}
-	  template <class T_=U, class ITER_>
+	  template <class T_=I, class ITER_>
 	  typename std::enable_if<
 	      (vec<T_>::size > std::iterator_traits<ITER_>::value_type::size),
 	      vec<T_> >::type
@@ -84,16 +80,16 @@ namespace detail
 			    return cvt<T_, MASK>(x, y);
 			}
 	  template <class S_>
-	  typename std::enable_if<vec<U>::size == vec<S_>::size, vec<U> >::type
+	  typename std::enable_if<vec<I>::size == vec<S_>::size, vec<I> >::type
 			operator ()(vec<S_> x) const
 			{
-			    return cvt<U, false, MASK>(x);
+			    return cvt<I, false, MASK>(x);
 			}
       };
 
-    //! vec<U> よりも要素数が多い入力SIMDベクトルを vec<U> に変換
+    //! vec<I> よりも要素数が多い入力SIMDベクトルを vec<I> に変換
       template <size_t N_, bool HI_>
-      struct generic_cvtup
+      struct generic_upArg
       {
 	  template <class ITER_>
 	  typename std::enable_if<
@@ -113,27 +109,12 @@ namespace detail
 			    return iter;
 			}
 	  template <class S_>
-	  typename std::enable_if<vec<cvt_upper_type<U, S_, MASK> >::size == N_,
-				  vec<cvt_upper_type<U, S_, MASK> > >::type
+	  typename std::enable_if<vec<cvt_upper_type<I, S_, MASK> >::size == N_,
+				  vec<cvt_upper_type<I, S_, MASK> > >::type
 			operator ()(vec<S_> x) const
 			{
-			    return cvt<cvt_upper_type<U, S_, MASK>,
+			    return cvt<cvt_upper_type<I, S_, MASK>,
 				       HI_, MASK>(x);
-			}
-      };
-
-    //! 変換関数が出力する vec<U> 型のSIMDベクトルをより要素数が多い vec<R> 型に変換
-      struct generic_cvtbelow
-      {
-	  template <class S_> vec<below_type<S_> >
-	  		operator ()(vec<S_> x) const
-			{
-			    return cvtbelow(x);
-			}
-	  template <class S_> vec<cvt_lower_type<R, S_, MASK> >
-	  		operator ()(vec<S_> x, vec<S_> y) const
-			{
-			    return cvtbelow(x, y);
 			}
       };
 
@@ -164,68 +145,90 @@ namespace detail
       };
 
     private:
-      template <class S_> static vec<below_type<S_> >
-		cvtbelow(vec<S_> x)
+    // 変換結果をconvert up
+      template <class TUPLE_>
+      typename std::enable_if<(vec<O>::size == tuple_head<TUPLE_>::size)>::type
+		upResult(const TUPLE_& x)
 		{
-		    return cvt<below_type<S_>, false, MASK>(x);
+		    ASSIGN()(*_out, cvt<O, false, MASK>(x));
+		    ++_out;
 		}
-      template <class S_> static vec<cvt_lower_type<R, S_, MASK> >
-		cvtbelow(vec<S_> x, vec<S_> y)
+      template <class TUPLE_>
+      typename std::enable_if<(vec<O>::size < tuple_head<TUPLE_>::size)>::type
+		upResult(const TUPLE_& x)
 		{
-		    return cvt<cvt_lower_type<R, S_, MASK>, MASK>(x, y);
+		    using U = cvt_upper_type<
+				  O, typename tuple_head<TUPLE_>::element_type,
+				  MASK>;
+		    
+		    upResult(cvt<U, false, MASK>(x));
+		    upResult(cvt<U, true,  MASK>(x));
 		}
-      template <class HEAD, class TAIL> static auto
-		cvtbelow(const boost::tuples::cons<HEAD, TAIL>& x)
-		    -> decltype(boost::tuples::cons_transform(
-				    generic_cvtbelow(), x))
+	  
+    // 変換結果をconvert down
+      template <class TUPLE_> static auto
+		downResult(const TUPLE_& x)
 		{
-		    return boost::tuples::cons_transform(generic_cvtbelow(), x);
+		    using S = typename tuple_head<TUPLE_>::element_type;
+		    using L = typename std::conditional<
+				  (vec<cvt_lower_type<O, S, MASK> >::size >
+				   vec<S>::size),
+				  S, cvt_lower_type<O, S, MASK> >::type;
+		    
+		    return cvt<L, false, MASK>(x);
 		}
-      template <class H1, class T1, class H2, class T2> static auto
-		cvtbelow(const boost::tuples::cons<H1, T1>& x,
-			 const boost::tuples::cons<H2, T2>& y)
-		    -> decltype(boost::tuples::cons_transform(
-				    generic_cvtbelow(), x, y))
+      template <class TUPLE_> static auto
+		downResult(const TUPLE_&x, const TUPLE_& y)
 		{
-		    return boost::tuples::cons_transform(generic_cvtbelow(),
-							 x, y);
+		    using S = typename tuple_head<TUPLE_>::element_type;
+
+		    return cvt<cvt_lower_type<O, S, MASK>, MASK>(x, y);
 		}
 
+    // 入力をconvert upし，変換結果をconvert down
       template <size_t N_, class TUPLE_,
-		typename std::enable_if<(N_ == vec<U>::size)>::type* = nullptr>
-      auto	cvtup_down(TUPLE_&& x)
+		typename std::enable_if<(N_ == vec<I>::size)>::type* = nullptr>
+      auto	upArg_downResult(TUPLE_&& x)
 		{
-		    return cvtbelow(_func(boost::tuples::cons_transform(
-					    generic_cvtdown(), x)));
+		    return downResult(_func(boost::tuples::cons_transform(
+						generic_downArg(), x)));
 		}
       template <size_t N_, class TUPLE_,
-		typename std::enable_if<(N_ > vec<U>::size)>::type* = nullptr>
-      auto	cvtup_down(TUPLE_&& x)
+		typename std::enable_if<(N_ > vec<I>::size)>::type* = nullptr>
+      auto	upArg_downResult(TUPLE_&& x)
 		{
-		    const auto	y = cvtup_down<N_/2>(
+		    const auto	y = upArg_downResult<N_/2>(
 					boost::tuples::cons_transform(
-					    generic_cvtup<N_/2, false>(), x));
-		    const auto	z = cvtup_down<N_/2>(
+					    generic_upArg<N_/2, false>(), x));
+		    const auto	z = upArg_downResult<N_/2>(
 					boost::tuples::cons_transform(
-					    generic_cvtup<N_/2, true >(), x));
-		    return cvtbelow(y, z);
+					    generic_upArg<N_/2, true >(), x));
+		    return downResult(y, z);
 		}
 
+    // 入力をconvert up
       template <size_t N_, class TUPLE_>
-      typename std::enable_if<(N_ == vec<R>::size)>::type
-		cvtup(TUPLE_&& x)
+      typename std::enable_if<(N_ == vec<I>::size && N_ > vec<O>::size)>::type
+		upArg(TUPLE_&& x)
 		{
-		    *_out = cvtup_down<N_>(x);
+		    upResult(_func(boost::tuples::cons_transform(
+				       generic_downArg(), x)));
+		}
+      template <size_t N_, class TUPLE_>
+      typename std::enable_if<(N_ == vec<O>::size)>::type
+		upArg(TUPLE_&& x)
+		{
+		    ASSIGN()(*_out, upArg_downResult<N_>(x));
 		    ++_out;
 		}
       template <size_t N_, class TUPLE_>
-      typename std::enable_if<(N_ > vec<U>::size && N_ > vec<R>::size)>::type
-		cvtup(TUPLE_&& x)
+      typename std::enable_if<(N_ > vec<I>::size && N_ > vec<O>::size)>::type
+		upArg(TUPLE_&& x)
 		{
-		    cvtup<N_/2>(boost::tuples::cons_transform(
-				    generic_cvtup<N_/2, false>(), x));
-		    cvtup<N_/2>(boost::tuples::cons_transform(
-				    generic_cvtup<N_/2, true >(), x));
+		    upArg<N_/2>(boost::tuples::cons_transform(
+				    generic_upArg<N_/2, false>(), x));
+		    upArg<N_/2>(boost::tuples::cons_transform(
+				    generic_upArg<N_/2, true >(), x));
 		}
 
     public:
@@ -238,11 +241,11 @@ namespace detail
 		    while (boost::get<0>(_t) != _end)
 		    {
 			constexpr size_t
-			    N = (max_size<ITER_TUPLE>::value > vec<R>::size ?
-				 max_size<ITER_TUPLE>::value : vec<R>::size);
+			    N = (max_size<ITER_TUPLE>::value > vec<O>::size ?
+				 max_size<ITER_TUPLE>::value : vec<O>::size);
 
-			cvtup<N>(boost::tuples::cons_transform(
-				     generic_cvtup<N, false>(), _t));
+			upArg<N>(boost::tuples::cons_transform(
+				     generic_upArg<N, false>(), _t));
 		    }
 		    return _out;
 		}
@@ -255,57 +258,60 @@ namespace detail
   };
 }	// namespace detail
 
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class FUNC, class OUT, class IN, class... IN_EXTRA> inline OUT
 transform(FUNC func, OUT out, IN in, IN end, IN_EXTRA... in_extra)
 {
-    detail::transformer<T, MASK, boost::tuple<IN, IN_EXTRA...>, OUT, FUNC>
+    detail::transformer<T, ASSIGN, MASK,
+			boost::tuple<IN, IN_EXTRA...>, OUT, FUNC>
 	trns(boost::make_tuple(in, in_extra...), end, out, func);
     return trns();
 }
     
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class ITER_TUPLE, class OUT, class FUNC> inline OUT
 transform(const ITER_TUPLE& ib, const ITER_TUPLE& ie, OUT out, FUNC func)
 {
-    detail::transformer<T, MASK, ITER_TUPLE, OUT, FUNC>
+    detail::transformer<T, ASSIGN, MASK, ITER_TUPLE, OUT, FUNC>
 	trns(ib, boost::get<0>(ie), out, func);
     return trns();
 }
     
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class ITER_TUPLE, class OUT, class FUNC> inline OUT
 transform(const fast_zip_iterator<ITER_TUPLE>& ib,
 	  const fast_zip_iterator<ITER_TUPLE>& ie, OUT out, FUNC func)
 {
-    return transform<T, MASK>(ib.get_iterator_tuple(),
-			      ie.get_iterator_tuple(), out, func);
+    return transform<T, ASSIGN, MASK>(ib.get_iterator_tuple(),
+				      ie.get_iterator_tuple(), out, func);
 }
     
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class OUT, class IN, class... IN_EXTRA> inline OUT
 copy(OUT out, IN in, IN end, IN_EXTRA... in_extra)
 {
-    detail::transformer<T, MASK, boost::tuple<IN, IN_EXTRA...>, OUT, identity>
+    detail::transformer<T, ASSIGN, MASK,
+			boost::tuple<IN, IN_EXTRA...>, OUT, identity>
 	trns(boost::make_tuple(in, in_extra...), end, out, identity());
     return trns();
 }
     
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class ITER_TUPLE, class OUT> inline OUT
 copy(const ITER_TUPLE& ib, const ITER_TUPLE& ie, OUT out)
 {
-    detail::transformer<T, MASK, ITER_TUPLE, OUT, identity>
+    detail::transformer<T, ASSIGN, MASK, ITER_TUPLE, OUT, identity>
 	trns(ib, boost::get<0>(ie), out, identity());
     return trns();
 }
     
-template <class T=void, bool MASK=false,
+template <class T=void, class ASSIGN=assign, bool MASK=false,
 	  class ITER_TUPLE, class OUT> inline OUT
 copy(const fast_zip_iterator<ITER_TUPLE>& ib,
      const fast_zip_iterator<ITER_TUPLE>& ie, OUT out)
 {
-    return copy<T, MASK>(ib.get_iterator_tuple(), ie.get_iterator_tuple(), out);
+    return copy<T, ASSIGN, MASK>(ib.get_iterator_tuple(),
+				 ie.get_iterator_tuple(), out);
 }
     
 }	// namespace simd
