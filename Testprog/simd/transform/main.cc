d845dfe679b30e83b57f267fa23f6f48a58df724
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2016-01-30 04:27:46 +0000

    Makefile updated.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1930 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- Testprog/simd/transform/main.cc
+++ Testprog/simd/transform/main.cc
@@ -22,7 +22,7 @@ struct sum
 
 namespace simd
 {
-template <class T, class U, class S0, class S1, class S2> void
+template <class T, class O, class S0, class S1, class S2> void
 doJob()
 {
     using	namespace std;
@@ -35,7 +35,7 @@ doJob()
 	       16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
     S2	z[] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
 	       16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-    U	u[32];
+    O	w[32];
 
     simd::copy<T>(ostream_iterator<target_type>(cout, "\n"),
 		  make_load_iterator(cbegin(x)),
@@ -45,15 +45,14 @@ doJob()
     cout << endl;
     
     simd::transform<T>(sum(),
-		       make_store_iterator(begin(u)),
+		       make_store_iterator(begin(w)),
 		       make_load_iterator(cbegin(x)),
 		       make_load_iterator(cend(x)),
 		       make_load_iterator(cbegin(y)),
 		       make_load_iterator(cbegin(z)));
 		       
-    std::copy(make_load_iterator(cbegin(u)),
-	      make_load_iterator(cend(u)),
-	      ostream_iterator<vec<U> >(cout, "\n"));
+    std::copy(make_load_iterator(cbegin(w)), make_load_iterator(cend(w)),
+	      ostream_iterator<vec<O> >(cout, "\n"));
 }
     
 }
@@ -62,6 +61,6 @@ doJob()
 int
 main()
 {
-    TU::simd::doJob<float, int16_t, int8_t, int16_t, int32_t>();
+    TU::simd::doJob<double, int8_t, int8_t, int16_t, int32_t>();
     return 0;
 }
