d7563497a11c2a27d5a7d0f51c84709759f6c998
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2016-06-15 22:07:43 +0000

    Test programs added.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@1976 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ testcam/main.cc
@@ -0,0 +1,55 @@
+/*
+ *  $Id: main.cc,v 1.4 2012-08-13 07:15:07 ueshiba Exp $
+ */
+#include <cstdlib>
+#include <iomanip>
+#include "TU/v/vIIDC++.h"
+#include "MyCmdWindow.h"
+
+/************************************************************************
+*  global functions							*
+************************************************************************/
+int
+main(int argc, char* argv[])
+{
+    using namespace	std;
+    using namespace	TU;
+    
+    v::App		vapp(argc, argv);
+    IIDCCamera::Speed	speed = IIDCCamera::SPD_400M;
+
+  // Parse command line.
+    extern char*	optarg;
+    for (int c; (c = getopt(argc, argv, "b")) != EOF; )
+	switch (c)
+	{
+	  case 'b':
+	    speed = IIDCCamera::SPD_800M;
+	    break;
+	}
+    extern int	optind;
+    u_int64_t	uniqId = 0;
+    if (optind < argc)
+	uniqId = strtoull(argv[optind], 0, 0);
+    
+  // Main job.
+    try
+    {
+	IIDCCamera	camera(IIDCCamera::Monocular, uniqId, speed);
+
+	v::MyCmdWindow<IIDCCamera, u_char>	myWin(vapp, camera);
+	vapp.run();
+
+	camera.stopContinuousShot();
+
+	cerr << "0x" << hex << setw(16) << setfill('0')
+	     << camera.globalUniqueId() << dec << ' ' << camera;
+    }
+    catch (exception& err)
+    {
+	cerr << err.what() << endl;
+	return 1;
+    }
+
+    return 0;
+}
---
+++ testcam2/main.cc
@@ -0,0 +1,84 @@
+/*
+ *  $Id: main.cc,v 1.2 2012-08-13 07:13:12 ueshiba Exp $
+ */
+#include <cstdlib>
+#include <iomanip>
+#include "TU/v/vIIDC++.h"
+#include "MyCmdWindow.h"
+
+namespace TU
+{
+/************************************************************************
+*  class CameraArray							*
+************************************************************************/
+class CameraArray : public Array<IIDCCamera*>
+{
+  public:
+    CameraArray(char* argv[], int argc, IIDCCamera::Speed speed)	;
+    ~CameraArray()							;
+};
+
+CameraArray::CameraArray(char* argv[], int argc, IIDCCamera::Speed speed)
+    :Array<IIDCCamera*>(argc)
+{
+    for (size_t i = 0; i < size(); ++i)
+	(*this)[i] = new IIDCCamera(IIDCCamera::Monocular,
+				    strtoull(argv[i], 0, 0), speed);
+}
+
+CameraArray::~CameraArray()
+{
+    for (size_t i = 0; i < size(); ++i)
+	delete (*this)[i];
+}
+
+}
+/************************************************************************
+*  global functions							*
+************************************************************************/
+int
+main(int argc, char* argv[])
+{
+    using namespace	std;
+    using namespace	TU;
+    
+    v::App		vapp(argc, argv);
+    IIDCCamera::Speed	speed = IIDCCamera::SPD_400M;
+    
+  // Parse command options.
+    extern char*	optarg;
+    for (int c; (c = getopt(argc, argv, "b")) != EOF; )
+	switch (c)
+	{
+	  case 'b':
+	    speed = IIDCCamera::SPD_800M;
+	    break;
+	}
+    
+    extern int		optind;
+    if (argc - optind == 0)
+    {
+	cerr << "One or more cameras must be specified!!" << endl;
+	return 1;
+    }
+    
+  // Main job.
+    try
+    {
+	CameraArray	cameras(argv + optind, argc - optind, speed);
+	for (size_t i = 0; i < cameras.dim(); ++i)
+	    cerr << "camera " << i << ": uniqId = "
+		 << hex << setw(16) << setfill('0')
+		 << cameras[i]->globalUniqueId() << dec << endl;
+	
+	v::MyCmdWindow<IIDCCamera, u_char>	myWin(vapp, cameras);
+	vapp.run();
+    }
+    catch (exception& err)
+    {
+	cerr << err.what() << endl;
+	return 1;
+    }
+
+    return 0;
+}
