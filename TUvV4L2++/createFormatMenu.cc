330b7304385d6c8778016e905e31b96e22a3a85a
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2016-11-28 01:21:12 +0000

    Cleaning up.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@2092 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUvV4L2++/createFormatMenu.cc
+++ TUvV4L2++/createFormatMenu.cc
@@ -12,72 +12,53 @@ namespace TU
 namespace v
 {
 /************************************************************************
-*  local data								*
-************************************************************************/
-static std::vector<MenuDef>			pixelFormatMenus;
-static std::list<std::vector<MenuDef> >		frameSizeMenusList;
-static std::list<std::vector<std::string> >	frameSizeLabelsList;
-    
-/************************************************************************
 *  global functions							*
 ************************************************************************/
 MenuDef*
 createFormatMenu(const V4L2Camera& camera)
 {
-    static const char*	setROILabel = "set ROI";
+    static std::vector<MenuDef>			pixelFormatMenus;
+    static std::list<std::vector<MenuDef> >	frameSizeMenusList;
+    static std::list<std::vector<std::string> >	frameSizeLabelsList;
+    static const char*				setROILabel = "set ROI";
     
-    BOOST_FOREACH (V4L2Camera::PixelFormat pixelFormat,
-		   camera.availablePixelFormats())
+    BOOST_FOREACH (auto pixelFormat, camera.availablePixelFormats())
     {
-      // この画素フォーマットに対応するメニュー項目を作る．
-	pixelFormatMenus.push_back(MenuDef());
-	MenuDef&	pixelFormatMenu = pixelFormatMenus.back();
-
-	pixelFormatMenu.label	= camera.getName(pixelFormat).c_str();
-	pixelFormatMenu.id	= pixelFormat;
-	pixelFormatMenu.checked = true;
-
       // この画素フォーマットがサポートする各フレームサイズに対応するメニュー項目を作る．
 	frameSizeMenusList.push_back(std::vector<MenuDef>());
-	std::vector<MenuDef>&	frameSizeMenus = frameSizeMenusList.back();
+	auto&	frameSizeMenus = frameSizeMenusList.back();
 	frameSizeLabelsList.push_back(std::vector<std::string>());
-	std::vector<std::string>&
-	    frameSizeLabels = frameSizeLabelsList.back();
-	BOOST_FOREACH (const V4L2Camera::FrameSize& frameSize,
+	auto&	frameSizeLabels = frameSizeLabelsList.back();
+	BOOST_FOREACH (const auto& frameSize,
 		       camera.availableFrameSizes(pixelFormat))
 	{
 	  // このフレームサイズに対応するメニュー項目を作る．
-	    frameSizeMenus.push_back(MenuDef());
-	    MenuDef&		frameSizeMenu = frameSizeMenus.back();
-	    const size_t	j = frameSizeMenus.size() - 1;
-
 	    std::ostringstream	s;
 	    s << frameSize;
 	    frameSizeLabels.push_back(s.str());
-	    frameSizeMenu.label = frameSizeLabels.back().c_str();
-	    frameSizeMenu.id	= j;
-	    frameSizeMenu.checked
-		= (camera.pixelFormat() == pixelFormat	    &&
-		   frameSize.width.involves(camera.width()) &&
-		   frameSize.height.involves(camera.height()));
-	    frameSizeMenu.submenu = noSub;
-	}
-	frameSizeMenus.push_back(MenuDef());
-	frameSizeMenus.back().label = 0;
 
-	pixelFormatMenu.submenu = &frameSizeMenus.front();
+	    frameSizeMenus.push_back(
+		MenuDef(frameSizeLabels.back().c_str(),
+			frameSizeMenus.size(),
+			(camera.pixelFormat() == pixelFormat	  &&
+			 frameSize.width.involves(camera.width()) &&
+			 frameSize.height.involves(camera.height()))));
+	}
+	frameSizeMenus.push_back(MenuDef());	// End of MenuDef
+
+      // この画素フォーマットに対応するメニュー項目を作る．
+	pixelFormatMenus.push_back(MenuDef(camera.getName(pixelFormat).c_str(),
+					   pixelFormat, true,
+					   &frameSizeMenus.front()));
     }
 
   // ROIを指定する項目を作る．
-    pixelFormatMenus.push_back(MenuDef());
-    MenuDef&	setROIMenu = pixelFormatMenus.back();
-    setROIMenu.label	= setROILabel;
-    setROIMenu.id	= V4L2Camera::UNKNOWN_PIXEL_FORMAT;
-    setROIMenu.checked	= false;
-    setROIMenu.submenu	= noSub;
-    
-    pixelFormatMenus.push_back(MenuDef());
-    pixelFormatMenus.back().label = 0;
+    pixelFormatMenus.push_back(MenuDef(setROILabel,
+				       V4L2Camera::UNKNOWN_PIXEL_FORMAT,
+				       false));
+
+    pixelFormatMenus.push_back(MenuDef());	// End of MenuDef
+
 
     return &pixelFormatMenus.front();
 }
