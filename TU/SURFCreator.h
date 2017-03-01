a998898641edbb26dfea3bddd3c9f7202463b4dd
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2017-03-01 06:42:18 +0000

    Cleaning up.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@2268 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TU/SURFCreator.h
+++ TU/SURFCreator.h
@@ -1,30 +1,4 @@
 /*
- *  平成21-22年（独）産業技術総合研究所 著作権所有
- *  
- *  創作者：植芝俊夫
- *
- *  本プログラムは（独）産業技術総合研究所の職員である植芝俊夫が創作し，
- *  （独）産業技術総合研究所が著作権を所有する秘密情報です．著作権所有
- *  者による許可なしに本プログラムを使用，複製，改変，第三者へ開示する
- *  等の行為を禁止します．
- *  
- *  このプログラムによって生じるいかなる損害に対しても，著作権所有者お
- *  よび創作者は責任を負いません。
- *
- *  Copyright 2009-2010.
- *  National Institute of Advanced Industrial Science and Technology (AIST)
- *
- *  Creator: Toshio UESHIBA
- *
- *  [AIST Confidential and all rights reserved.]
- *  This program is confidential. Any using, copying, changing or
- *  giving any information concerning with this program to others
- *  without permission by the copyright holder are strictly prohibited.
- *
- *  [No Warranty.]
- *  The copyright holder or the creator are not responsible for any
- *  damages caused by using this program.
- *
  *  $Id: SURFCreator.h,v 1.16 2012-01-24 02:44:54 ueshiba Exp $
  */
 #ifndef __TU_SURFCREATOR_H
@@ -331,11 +305,11 @@ SURFCreator::detectFeatures(const Image<T>& image, Inserter<F>& insert) const
 template <class ITER> void
 SURFCreator::makeDescriptors(ITER begin, ITER end) const
 {
+#if defined(USE_TBB)
     typedef typename std::iterator_traits<ITER>::value_type
 							feature_type;
     typedef typename feature_type::value_type		feature_value_type;
 
-#if defined(USE_TBB)
     tbb::parallel_for(tbb::blocked_range<ITER>(begin, end, 1),
 		      Apply<ITER>(
 			  *this,
