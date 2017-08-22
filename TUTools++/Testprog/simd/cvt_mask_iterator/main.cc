cb077b767b1630cc792a46070d8ad0f0c4387ebf
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2017-08-22 06:07:55 +0000

    Cleaning up.
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/src@2560 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- TUTools++/Testprog/simd/cvt_mask_iterator/main.cc
+++ TUTools++/Testprog/simd/cvt_mask_iterator/main.cc
@@ -38,57 +38,57 @@ main()
     using namespace	std;
     using namespace	TU;
     
-    cerr << "--- src: u_int8_t, dst: unsinged ---" << endl;
-    simd::doJob<u_int8_t,  u_int8_t >();
-    simd::doJob<u_int8_t,  u_int16_t>();
-    simd::doJob<u_int8_t,  u_int32_t>();
-    simd::doJob<u_int8_t,  u_int64_t>();
+    cerr << "--- src: uint8_t, dst: unsinged ---" << endl;
+    simd::doJob<uint8_t,  uint8_t >();
+    simd::doJob<uint8_t,  uint16_t>();
+    simd::doJob<uint8_t,  uint32_t>();
+    simd::doJob<uint8_t,  uint64_t>();
 
-    cerr << "--- src: u_int16_t, dst: unsinged ---" << endl;
-    simd::doJob<u_int16_t, u_int8_t >();
-    simd::doJob<u_int16_t, u_int16_t>();
-    simd::doJob<u_int16_t, u_int32_t>();
-    simd::doJob<u_int16_t, u_int64_t>();
+    cerr << "--- src: uint16_t, dst: unsinged ---" << endl;
+    simd::doJob<uint16_t, uint8_t >();
+    simd::doJob<uint16_t, uint16_t>();
+    simd::doJob<uint16_t, uint32_t>();
+    simd::doJob<uint16_t, uint64_t>();
 
-    cerr << "--- src: u_int32_t, dst: unsinged ---" << endl;
-    simd::doJob<u_int32_t, u_int8_t >();
-    simd::doJob<u_int32_t, u_int16_t>();
-    simd::doJob<u_int32_t, u_int32_t>();
-    simd::doJob<u_int32_t, u_int64_t>();
+    cerr << "--- src: uint32_t, dst: unsinged ---" << endl;
+    simd::doJob<uint32_t, uint8_t >();
+    simd::doJob<uint32_t, uint16_t>();
+    simd::doJob<uint32_t, uint32_t>();
+    simd::doJob<uint32_t, uint64_t>();
 
-    cerr << "--- src: u_int64_t, dst: unsinged ---" << endl;
+    cerr << "--- src: uint64_t, dst: unsinged ---" << endl;
 #if defined(NEON)
-    simd::doJob<u_int64_t, u_int8_t >();
-    simd::doJob<u_int64_t, u_int16_t>();
-    simd::doJob<u_int64_t, u_int32_t>();
+    simd::doJob<uint64_t, uint8_t >();
+    simd::doJob<uint64_t, uint16_t>();
+    simd::doJob<uint64_t, uint32_t>();
 #endif
-    simd::doJob<u_int64_t, u_int64_t>();
+    simd::doJob<uint64_t, uint64_t>();
 
 #if defined(SSE2) || defined(NEON)
     cerr << "--- src: unsigned, dst: float ---" << endl;
-    simd::doJob<u_int8_t,  float>();
-    simd::doJob<u_int16_t, float>();
+    simd::doJob<uint8_t,  float   >();
+    simd::doJob<uint16_t, float   >();
 #  if !defined(AVX) || defined(AVX2)
-    simd::doJob<u_int32_t, float>();
+    simd::doJob<uint32_t, float   >();
 #  endif
     cerr << "--- src: float, dst: unsigned ---" << endl;
-    simd::doJob<float, u_int8_t >();
-    simd::doJob<float, u_int16_t>();
+    simd::doJob<float,    uint8_t >();
+    simd::doJob<float,    uint16_t>();
 #  if !defined(AVX) || defined(AVX2)
-    simd::doJob<float, u_int32_t>();
+    simd::doJob<float,    uint32_t>();
 #  endif
 #endif
 
 #if defined(SSE2)
     cerr << "--- src: unsigned, dst: double ---" << endl;
-    simd::doJob<u_int8_t,  double>();
-    simd::doJob<u_int16_t, double>();
-    simd::doJob<u_int32_t, double>();
+    simd::doJob<uint8_t,  double  >();
+    simd::doJob<uint16_t, double  >();
+    simd::doJob<uint32_t, double  >();
 #  if !defined(AVX) || defined(AVX2)
-    simd::doJob<u_int64_t, double>();
+    simd::doJob<uint64_t, double  >();
 
     cerr << "--- src: double, dst: unsigned ---" << endl;
-    simd::doJob<double, u_int64_t>();
+    simd::doJob<double, uint64_t  >();
 #  endif
 #endif
 
